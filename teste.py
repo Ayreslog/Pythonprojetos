0ef6df9e45329b5e833942a4393151585eb195cc
Ayres Costa Filho <111866394+Ayreslog@users.noreply.github.com>
2023-06-15 15:33:31 -0300

    Add files via upload
    Este arquivo é um leitor que usa a fala do Google

---
+++ teste.py
@@ -0,0 +1,17 @@
+import pyttsx3
+
+# Carrega a lib #
+engine = pyttsx3.init()
+while True:
+# Texto que a engine vai falar #
+ engine.say('OLÁ TIGRADA!!!')
+ #engine.say('O FELIPE BÉÉTI É UM TIGRE???')
+ #engine.say('O VEIINHO DÉLCIO É O REI DOS TIGRES!!!')
+ engine.say('O LEONARDO LEANDRO É UM TIGRE!!!')
+ engine.say('A MARIA É TIGRINHA!!!')
+ engine.say('TODO MUNDO AQUI É TIGRE!!!')
+ #engine.say('O WILILIAM É UM TIGRE!!!')
+ #engine.say('O CASSIANO É UM TIGRE!!!')
+ engine.say('SÓ O AYRES NÃO É TIGRE!!!')
+# Executa #
+ engine.runAndWait()
